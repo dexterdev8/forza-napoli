acb72d5568fe699c40c224039b57e8705574051f
dexterdev8 <ioan.oara@protonmail.com>
2022-02-28 19:19:29 +0100

    fix camera message format

--- src/camera.py
+++ src/camera.py
@@ -8,12 +8,13 @@ class Camera:
         self.channel = rabbitmq_producer()
         self.routing_key = route_destination
         self.signals = [signals_initial_state for _ in range(number_signals)]
+        self.simulate_trigger_signal()
 
     def simulate_trigger_signal(self):
         while True:
             for i in range(len(self.signals)):
                 if self.signals[i]:
-                    message = "{}:{}".format(self.signals[i], self.name)
+                    message = "{}:{}:{}".format(self.name, i, self.signals[i])
                     self.signals[i] = not self.signals[i]
                     self.channel.basic_publish(exchange="topic_logs", routing_key=self.routing_key, body=message)
                     print(" [x] %s Sent %r:%r" % (self.name, self.routing_key, message))
@@ -21,9 +22,24 @@ class Camera:
 
 
 if __name__ == "__main__":
-    C1 = Camera(
+    # Example of the ingredients quality camera
+    C_ingredient_quality = Camera(
         name="ingredients_monitoring",
         route_destination="ingredients.chef.robot",
         number_signals=1,
-        signals_initial_state=False,
+        signals_initial_state=True,
+    )
+
+    C_shelf_monitor = Camera(
+        name="shelf_monitoring",
+        route_destination="delivery.prod.robot",
+        number_signals=8,
+        signals_initial_state=True,
+    )
+
+    C_oven_monitor = Camera(
+        name="oven_monitoring",
+        route_destination="delivery.prod.robot",
+        number_signals=1,
+        signals_initial_state=True,
     )
